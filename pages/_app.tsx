c65952a95e29432501b4fda207b16069fb80eefb
Thomas Clark <thomas.clark@bigtincan.com>
2022-06-03 16:29:31 +1000

    removed boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,7 @@ import { MantineProvider } from '@mantine/core'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: 'light',
-        breakpoints: {
-          xs: 500,
-          sm: 800,
-          md: 1000,
-          lg: 1200,
-          xl: 1400,
-        },
-      }}
-    >
+    <MantineProvider withGlobalStyles withNormalizeCSS>
       <Component {...pageProps} />
     </MantineProvider>
   )
