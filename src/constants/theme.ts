06f091dc10120b54fee12a45ab0e6aebae41efe5
Thomas Clark <thomas.clark@bigtincan.com>
2022-06-03 16:33:49 +1000

    src folder, use preferred color scheme

---
+++ src/constants/theme.ts
@@ -0,0 +1,7 @@
+import { MantineTheme } from "@mantine/core";
+
+const theme:Partial<MantineTheme> = {
+	primaryColor: 'blue'
+}
+
+export default theme;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
+import { useColorScheme } from '@mantine/hooks'
+import { theme } from '../constants'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const preferredColorScheme = useColorScheme()
+  return (
+    <MantineProvider
+      theme={{
+        ...theme,
+        colorScheme: preferredColorScheme,
+      }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
+}
+
+export default MyApp
