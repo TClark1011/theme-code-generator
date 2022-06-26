4462a600e3f66139a2f8573c71e50f5a8e4bf23b
Thomas Clark <thomas.clark@bigtincan.com>
2022-06-27 09:43:01 +1000

    fix: 🐛 default code label is now always applied
    fixed a bug where the default code label would not be applied if you
    went to the second form step without changing anything on the first
    step. NOTE: This bug went unnoticed because react 18's new "feature" for
    detecting "unintended side effects" also messes with how certain
    lifecycle hooks run, which caused the correct label to always get
    applied in dev mode, but not in production, thanks react  team very cool

--- src/store/StoreSideEffects.tsx
+++ src/store/StoreSideEffects.tsx
@@ -2,16 +2,19 @@ import { selectSelectedScaleType } from '$/store/selectors';
 import { useStoreDispatch, useStoreSelector } from '$/store/storeHooks';
 import { resetPresetSelection, updateCodeLabel } from '$code-generation';
 import { useDidUpdate } from '@mantine/hooks';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 const StoreSideEffects: FC = () => {
   const dispatch = useStoreDispatch();
   const selectedScaleType = useStoreSelector(selectSelectedScaleType);
 
+  useEffect(() => {
+    dispatch(updateCodeLabel(selectedScaleType)); // apply default code label
+  }, [selectedScaleType, dispatch]);
+
   useDidUpdate(() => {
     // # When a new scale type is selected ...
     dispatch(resetPresetSelection); // clear preset selection
-    dispatch(updateCodeLabel(selectedScaleType)); // apply default code label
   }, [selectedScaleType]);
 
   return null;
