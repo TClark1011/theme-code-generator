a159de4083e1ed91d75785f7295023cb5dd91e2e
Thomas Clark <thomas.clark@bigtincan.com>
2022-07-09 14:33:49 +1000

    fix: 🐛 fixed analytics (again)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,11 +10,6 @@ export default class _Document extends Document {
     return (
       <Html>
         <Head>
-          <script
-            defer
-            data-domain="designvariables.com"
-            src="https://plausible.io/js/plausible.js"
-          />
           <link
             href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
             rel="stylesheet"
