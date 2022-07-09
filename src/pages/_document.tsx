aebc767674bcd59260aebe9b5bcb48d62351e635
Thomas Clark <thomas.clark@bigtincan.com>
2022-07-09 13:45:30 +1000

    feat: 🎸 added analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { createGetInitialProps } from '@mantine/next';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 const getInitialProps = createGetInitialProps();
 
@@ -10,6 +11,11 @@ export default class _Document extends Document {
     return (
       <Html>
         <Head>
+          <Script
+            defer
+            data-domain="designvariables.com"
+            src="https://plausible.io/js/plausible.js"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
             rel="stylesheet"
